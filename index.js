526331bbef213db0a8310a446bf7e0a941bd538c
Salman-S7 <sammyzed1505@gmail.com>
2024-06-13 15:28:33 +0530

    chaged backend route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.status(200).json({messege : "Hello how are you???"})
+    res.status(200).json({messege : "Hello World!!!"})
 })
 
 app.listen(3000, () => {
